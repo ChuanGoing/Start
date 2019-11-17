5a00307321134e4e8a12a1eef5002ffc444f93fe
e370101152 <e370101152@aliyun.com>
2019-11-17 23:08:35 +0800

    注册Unitofwork

---
+++ ChuanGoing.Base/BaseModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using ChuanGoing.Base.Ioc;
+using ChuanGoing.Base.Uow;
+
+namespace ChuanGoing.Base
+{
+    public class BaseModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //builder.RegisterAssembly(ThisAssembly);
+            builder.RegisterType<UnitOfWork>().AsImplementedInterfaces().InstancePerLifetimeScope();
+        }
+    }
+}
